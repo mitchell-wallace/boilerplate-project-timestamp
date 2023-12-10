2e8061f89f133b7e2aa701073bb7d5449d6d5a9b
Mitchell Wallace <38238516+mitchell-wallace@users.noreply.github.com>
2023-12-10 12:03:14 +1100

    All tests passed
    Some code still needs to be cleaned up

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ app.get("/", function (req, res) {
 // ----- TEST -----
 
 let demoDateString = '1451001600000';
-let demoDate = new Date(demoDateString);
+let demoDate = new Date(demoDateString); // invalid date
 
 let demoDateString2 = '2015-12-25';
-let demoDate2 = new Date(demoDateString2);
+let demoDate2 = new Date(demoDateString2); // success
 
 let demoDateNo3= 1451001600000;
-let demoDate3 = new Date(demoDateNo3);
+let demoDate3 = new Date(demoDateNo3); // success
 
 if (demoDate.valueOf() == demoDate2.valueOf())
 {
@@ -37,6 +37,31 @@ else
 {
   console.log('false');
 }
+
+if (isNaN(demoDate.valueOf()))
+  { console.log("\t>>NaN test passed");}
+if (Number.isNaN(demoDate.valueOf()))
+  { console.log("\t>>NaN number test passed");}
+if (demoDate.toString() == "Invalid Date")
+  { console.log("\t>>string test passed");}
+
+if (isNaN(demoDate2.valueOf()))
+  { console.log("\t>>NaN test false positive");}
+if (Number.isNaN(demoDate2.valueOf()))
+  { console.log("\t>>NaN number test false positive");}
+if (demoDate2.toString() == "Invalid Date")
+  { console.log("\t>>string test false positive");}
+
+  console.log('value: ' + demoDate.valueOf());
+  console.log('string: ' + demoDate.toString());
+  console.log('direct: ' + demoDate);
+
+  console.log('2value: ' + demoDate2.valueOf());
+  console.log('2string: ' + demoDate2.toString());
+  console.log('2direct: ' + demoDate2);
+  
+if (demoDate == new Date('xyz'))
+  { console.log("\t>>known invalid date object test passed");}
 
 console.log({"unix": demoDate2[Symbol.toPrimitive]('number'),
 "utc" : demoDate2.toUTCString()});
@@ -54,29 +79,46 @@ app.get("/api/:date?", (req, res) => {
   let thisDate;
 
   try {
-    let dateInput = req.params.date.toString();
-    if (dateInput = '') {
-      dateInput = new Date();
-    }
 
-    if (!isNaN(dateInput)) 
+    // if no date parameter present, return current time
+    if (!req.params.date) 
     {
-      dateInput = parseInt(dateInput);
+      thisDate = new Date();
+    }
+    // if date parameter present, process it
+    else
+    {
+      let dateInput = req.params.date.toString();
+  
+      // if dateInput is a number, then we handle it as number not string
+      if (!isNaN(dateInput)) 
+      {
+        dateInput = parseInt(dateInput);
+      }
+  
+      // attempt to create a date from this string
+      thisDate = new Date(dateInput);
+  
+      // this will catch an invalid date and return a response
+      if (isNaN(thisDate.valueOf())) {
+        res.json({"error" : "Invalid Date"});
+        return;
+      }
     }
 
-    thisDate = new Date(dateInput);
-    // Code to execute if the line above doesn't throw an error
-    if (thisDate.valueOf() == NaN) {
-      res.json({"error" : "Invalid Date"});
-      return;
-    }
-  } catch (error) {
-    // Code to handle the error
-    console.log(error);
+    // for an empty or a valid date, this will return a response
+    res.json({"unix": thisDate[Symbol.toPrimitive]('number'),
+      "utc" : thisDate.toUTCString()});
+    return;
   }
 
-  res.json({"unix": thisDate[Symbol.toPrimitive]('number'),
-    "utc" : thisDate.toUTCString()});
+  catch (error) {
+    // catch and log any errors encountered above and return error message
+    // this should be different to "invalid date" because that shouldn't reach here
+    console.log(error);
+    res.json({"error" : "Server Error-a"});
+    return;
+  }
 });
 
 
