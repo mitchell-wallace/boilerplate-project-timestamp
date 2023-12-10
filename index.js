b1333b2bfb4c97158896e693e085d4fdf3f0c75e
Mitchell Wallace <38238516+mitchell-wallace@users.noreply.github.com>
2023-12-10 11:12:11 +1100

    5/8 tests passing
    should have committed sooner, somewhat underestimated this project

--- index.js
+++ index.js
@@ -18,10 +18,65 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// ----- TEST -----
+
+let demoDateString = '1451001600000';
+let demoDate = new Date(demoDateString);
+
+let demoDateString2 = '2015-12-25';
+let demoDate2 = new Date(demoDateString2);
+
+let demoDateNo3= 1451001600000;
+let demoDate3 = new Date(demoDateNo3);
+
+if (demoDate.valueOf() == demoDate2.valueOf())
+{
+  console.log('true');
+}
+else 
+{
+  console.log('false');
+}
+
+console.log({"unix": demoDate2[Symbol.toPrimitive]('number'),
+"utc" : demoDate2.toUTCString()});
+
+// ---- END-TEST -----
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+// :date
+app.get("/api/:date?", (req, res) => {
+
+  let thisDate;
+
+  try {
+    let dateInput = req.params.date.toString();
+    if (dateInput = '') {
+      dateInput = new Date();
+    }
+
+    if (!isNaN(dateInput)) 
+    {
+      dateInput = parseInt(dateInput);
+    }
+
+    thisDate = new Date(dateInput);
+    // Code to execute if the line above doesn't throw an error
+    if (thisDate.valueOf() == NaN) {
+      res.json({"error" : "Invalid Date"});
+      return;
+    }
+  } catch (error) {
+    // Code to handle the error
+    console.log(error);
+  }
+
+  res.json({"unix": thisDate[Symbol.toPrimitive]('number'),
+    "utc" : thisDate.toUTCString()});
 });
 
 
